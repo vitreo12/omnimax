b1ad1945a650059771fb40c5ce167213fc019116
Francesco Cameli <francesco.cameli95@gmail.com>
2020-03-15 15:33:48 +0000

    Added (empty) buffer module

---
+++ omnimax_lang/omnimax_lang.nim
@@ -0,0 +1,11 @@
+#Import omni_lang. This will also make it available in omnimax_buffer for omni.alloc
+import omni_lang
+
+#Export it
+export omni_lang
+
+#Import SC's Buffer module
+import omnimax_lang/lib/buffer/omnimax_buffer
+
+#Export it
+export omnimax_buffer
