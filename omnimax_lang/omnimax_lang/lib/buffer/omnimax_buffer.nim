dbd1dc9f72568b713dfb7fb5db68b08226c18124
Francesco Cameli <francesco.cameli95@gmail.com>
2020-03-23 12:49:20 +0000

    Finished buffer interface

--- omnimax_lang/omnimax_lang/lib/buffer/omnimax_buffer.nim
+++ omnimax_lang/omnimax_lang/lib/buffer/omnimax_buffer.nim
@@ -1,35 +1,30 @@
-import omni_lang
+#[ All these functions are defined in the Max object cpp file ]#
 
-#cpp file to compile together. Should I compile it ahead and use the link pragma on the .o instead?
-{.compile: "omnimax_buffer.cpp".}
+#Retrieve buffer_ref*, or initialize one with random name
+proc init_buffer_at_inlet(max_object : pointer, inlet : cint) : pointer {.importc, cdecl.}
 
-#Flags to cpp compiler
-{.passC: "-O3".}
+#Retrive buffer_obj*
+proc get_buffer_obj(buffer_ref : pointer) : pointer {.importc, cdecl.}
 
-#Wrapping of cpp functions
-proc get_buffer_Max(buf : pointer, fbufnum : cfloat) : pointer {.importc, cdecl.}
+#Lock / Unlock
+proc lock_buffer_Max(buffer_obj : pointer)   : ptr float {.importc, cdecl.}
+proc unlock_buffer_Max(buffer_obj : pointer) : void      {.importc, cdecl.}
 
-proc unlock_buffer_Max(buf : pointer) : void {.importc, cdecl.}
+#Utilities
+proc get_frames_buffer_Max(buffer_obj : pointer)     : clong   {.importc, cdecl.}
+proc get_samples_buffer_Max(buffer_obj : pointer)    : clong   {.importc, cdecl.}
+proc get_channels_buffer_Max(buffer_obj : pointer)   : clong   {.importc, cdecl.}
+proc get_samplerate_buffer_Max(buffer_obj : pointer) : cdouble {.importc, cdecl.}
 
-proc get_float_value_buffer_Max(buf : pointer, index : clong, channel : clong) : cfloat {.importc, cdecl.}
-
-proc set_float_value_buffer_Max(buf : pointer, value : cfloat, index : clong, channel : clong) : void {.importc, cdecl.}
-
-proc get_frames_buffer_Max(buf : pointer) : cint {.importc, cdecl.}
-
-proc get_samples_buffer_Max(buf : pointer) : cint {.importc, cdecl.}
-
-proc get_channels_buffer_Max(buf : pointer) : cint {.importc, cdecl.}
-
-proc get_samplerate_buffer_Max(buf : pointer) : cdouble {.importc, cdecl.}
-
-proc get_sampledur_buffer_Max(buf : pointer) : cdouble {.importc, cdecl.}
+#proc get_sampledur_buffer_Max(buffer_obj : pointer) : cdouble {.importc, cdecl.}
 
 type
     Buffer_obj = object
-        buf_ptr      : pointer                      #pointer to t_buffer_ref
-        buf_data_ptr : ptr UncheckedArray[float32]  #actual float* data
-        input_num*   : int                          #need to export it in order to be retrieved with the ins_Nim[buffer.input_num][0] syntax for get_buffer.
+        max_object  : pointer                      #pointer to max's t_object
+        buffer_ref  : pointer                      #pointer to t_buffer_ref
+        buffer_obj  : pointer                      #pointer to t_buffer_obj
+        buffer_data : ptr UncheckedArray[float32]  #actual float* data
+        input_num*  : int                          #need to export it in order to be retrieved with the ins_Nim[buffer.input_num][0] syntax for get_buffer.
 
     Buffer* = ptr Buffer_obj
 
@@ -39,10 +34,20 @@ const
     lower_exceed_input_error  = "ERROR: Buffer: input %d out of bounds. Minimum input number is 1.\n"
 
 #Init buffer
-proc innerInit*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, omni_inputs : int) : Buffer =
+proc innerInit*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, omni_inputs : int, buffer_interface : pointer) : Buffer =
+    #Just allocate the object. All max related init are done in get_buffer
     result = cast[Buffer](omni_alloc(cast[culong](sizeof(Buffer_obj))))
 
-    #If these checks fail set to buf_ptr to nil, which will invalidate the Buffer (the get_buffer_Max would just return null)
+    #Assign the max object the buffer refers to
+    result.max_object = buffer_interface
+
+    #1 should be 0, 2 1, 3 2, etc... 32 31
+    let real_input_num = int(input_num) - int(1)
+    result.input_num   = real_input_num
+
+    #Create the buffer_ref or get one if it was already created in Max from the args
+    result.buffer_ref = init_buffer_at_inlet(result.max_object, cint(real_input_num))
+
     if input_num > omni_inputs:
         omni_print(exceeding_max_ugen_inputs, omni_inputs)
 
@@ -52,21 +57,39 @@ proc innerInit*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, omn
     elif input_num < 1:
         omni_print(lower_exceed_input_error, input_num)
 
-#Template which also uses the const omni_inputs, which belongs to the omni dsp new module. It will string substitute Buffer.init(1) with initInner(Buffer, 1, omni_inputs)
+#Template which also uses the const omni_inputs, which belongs to the omni dsp new module. It will string substitute Buffer.init(1) with initInner(Buffer, 1, omni_inputs, ugen.buffer_interface_let)
 template new*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S) : untyped =
-    innerInit(Buffer, input_num, omni_inputs) #omni_inputs belongs to the scope of the dsp module
+    innerInit(Buffer, input_num, omni_inputs, buffer_interface) #omni_inputs AND user_buffer_interface belong to the scope of the dsp module and the body of the init function
 
 proc destructor*(buffer : Buffer) : void =
     print("Calling Buffer's destructor")
     let buffer_ptr = cast[pointer](buffer)
     omni_free(buffer_ptr)
 
-#Called at start of perform. If supernova is active, this will also lock the buffer.
-proc get_buffer*(buffer : Buffer, fbufnum : float32) : void =
-    discard
+#Called at start of perform. This should also lock the buffer.
+proc get_buffer*(buffer : Buffer, fbufnum : float32) : bool =
+    let buffer_ref = buffer.buffer_ref
+    if isNil(buffer_ref):
+        #omni_print("INVALID BUFFER_REF")
+        return false
+
+    let buffer_obj    = get_buffer_obj(buffer_ref)
+    buffer.buffer_obj = buffer_obj
+    if isNil(buffer_obj):
+        #omni_print("INVALID BUFFER_OBJ")
+        return false
+    
+    let buffer_data    = cast[ptr UncheckedArray[float32]](lock_buffer_Max(buffer_obj))
+    buffer.buffer_data = buffer_data
+    if isNil(cast[pointer](buffer_data)):
+        #omni_print("INVALID DATA")
+        return false
+    
+    #All good, go on with the perform function
+    return true
 
 proc unlock_buffer*(buffer : Buffer) : void =
-    unlock_buffer_Max(cast[pointer](buffer.buf_data_ptr))
+    unlock_buffer_Max(buffer.buffer_obj)
 
 ##########
 # GETTER #
@@ -74,11 +97,28 @@ proc unlock_buffer*(buffer : Buffer) : void =
 
 #1 channel
 proc `[]`*[I : SomeNumber](a : Buffer, i : I) : float32 =
-    return get_float_value_buffer_Max(a.buf_data_ptr, clong(i), clong(0))
+    let 
+        buf_data = a.buffer_data
+        buf_obj  = a.buffer_obj
+        index    = int(i)
+
+    if index >= 0 and index < int(get_frames_buffer_Max(buf_obj)):
+        return buf_data[index]
+
+    return float32(0.0)
 
 #more than 1 channel
 proc `[]`*[I1 : SomeNumber, I2 : SomeNumber](a : Buffer, i1 : I1, i2 : I2) : float32 =
-    return get_float_value_buffer_Max(a.buf_data_ptr, clong(i1), clong(i2))
+    let 
+        buf_data = a.buffer_data
+        buf_obj  = a.buffer_obj
+        channel  = int(i2)
+        index    = (int(i1) * channel) + channel
+
+    if index >= 0 and index < int(get_samples_buffer_Max(buf_obj)):
+        return buf_data[index]
+    
+    return float32(0.0)
 
 ##########
 # SETTER #
@@ -86,32 +126,48 @@ proc `[]`*[I1 : SomeNumber, I2 : SomeNumber](a : Buffer, i1 : I1, i2 : I2) : flo
 
 #1 channel
 proc `[]=`*[I : SomeNumber, S : SomeNumber](a : Buffer, i : I, x : S) : void =
-    set_float_value_buffer_Max(a.buf_data_ptr, cfloat(x), clong(i), clong(0))
+    var buf_data = a.buffer_data
+    
+    let 
+        buf_obj = a.buffer_obj
+        index   = int(i)
+        value   = float32(x)
+
+    if index >= 0 and index < int(get_frames_buffer_Max(buf_obj)):
+        buf_data[index] = value
 
 #more than 1 channel
 proc `[]=`*[I1 : SomeNumber, I2 : SomeNumber, S : SomeNumber](a : Buffer, i1 : I1, i2 : I2, x : S) : void =
-    set_float_value_buffer_Max(a.buf_data_ptr, cfloat(x), clong(i1), clong(i2))
+    var buf_data = a.buffer_data
+    let 
+        buf_obj = a.buffer_obj
+        channel = int(i2)
+        index   = (int(i1) * channel) + channel
+        value   = float32(x)
+
+    if index >= 0 and index < int(get_samples_buffer_Max(buf_obj)):
+        buf_data[index] = value
 
 #########
 # INFOS #
 #########
 
 #length of each frame in buffer
-proc len*(buffer : Buffer) : cint =
-    return get_frames_buffer_Max(buffer.buf_data_ptr)
+proc len*(buffer : Buffer) : int {.inline.} =
+    return int(get_frames_buffer_Max(buffer.buffer_obj))
 
-#Returns total size (buf_data_ptr->samples)
-proc size*(buffer : Buffer) : cint =
-    return get_samples_buffer_Max(buffer.buf_data_ptr)
+#Returns total size (frames * channels)
+proc size*(buffer : Buffer) : int {.inline.} =
+    return int(get_samples_buffer_Max(buffer.buffer_obj))
 
 #Number of channels
-proc nchans*(buffer : Buffer) : cint =
-    return get_channels_buffer_Max(buffer.buf_data_ptr)
+proc nchans*(buffer : Buffer) : int {.inline.} =
+    return int(get_channels_buffer_Max(buffer.buffer_obj))
 
 #Samplerate (float64)
-proc samplerate*(buffer : Buffer) : cdouble =
-    return get_samplerate_buffer_Max(buffer.buf_data_ptr)
+proc samplerate*(buffer : Buffer) : float {.inline.} =
+    return float(get_samplerate_buffer_Max(buffer.buffer_obj))
 
 #Sampledur (Float64)
-proc sampledur*(buffer : Buffer) : cdouble =
-    return get_sampledur_buffer_Max(buffer.buf_data_ptr)
+#proc sampledur*(buffer : Buffer) : float =
+#    return float(get_sampledur_buffer_Max(buffer.buffer_obj))
