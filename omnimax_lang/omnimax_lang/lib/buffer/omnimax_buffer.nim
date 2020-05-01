3431380d60d1d0804970e4da2091b3697dfc020f
Francesco <francesco.cameli95@gmail.com>
2020-05-01 21:31:23 +0100

    Support for new ugen_call_type

--- omnimax_lang/omnimax_lang/lib/buffer/omnimax_buffer.nim
+++ omnimax_lang/omnimax_lang/lib/buffer/omnimax_buffer.nim
@@ -56,7 +56,11 @@ type
     Buffer* = ptr Buffer_obj
 
 #Init buffer
-proc struct_init_inner*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, buffer_interface : pointer, ugen_auto_mem : ptr OmniAutoMem) : Buffer {.inline.} =
+proc struct_init_inner*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, buffer_interface : pointer, ugen_auto_mem : ptr OmniAutoMem, ugen_call_type : typedesc[CallType] = InitCall) : Buffer {.inline.} =
+    #Trying to allocate in perform block! nonono
+    when ugen_call_type is PerformCall:
+        {.fatal: "attempting to allocate memory in the `perform` or `sample` blocks for `struct Buffer`".}
+
     #Just allocate the object. All max related init are done in get_buffer
     result = cast[Buffer](omni_alloc(culong(sizeof(Buffer_obj))))
 
@@ -104,7 +108,7 @@ macro checkInputNum*(input_num_typed : typed, omni_inputs_typed : typed) : untyp
 #Template which also uses the const omni_inputs, which belongs to the omni dsp new module. It will string substitute Buffer.init(1) with initInner(Buffer, 1, omni_inputs, ugen.buffer_interface_let)
 template new*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S) : untyped =
     checkInputNum(input_num, omni_inputs)
-    struct_init_inner(Buffer, input_num, buffer_interface, ugen_auto_mem) #omni_inputs AND buffer_interface belong to the scope of the dsp module and the body of the init function
+    struct_init_inner(Buffer, input_num, buffer_interface, ugen_auto_mem, ugen_call_type) #omni_inputs AND buffer_interface belong to the scope of the dsp module and the body of the init function
 
 #Register child so that it will be picked up in perform to run get_buffer / unlock_buffer
 proc checkValidity*(obj : Buffer, ugen_auto_buffer : ptr OmniAutoMem) : bool =
